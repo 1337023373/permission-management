a59f9cec705507b3835a0683e27b1c7810e13f01
liukejun <1337023373@qq.com>
2025-05-22 11:41:28 +0800

    feat：搭建项目结构，添加logback

---
+++ src/main/java/com/hengheng/PermissionApplication.java
@@ -0,0 +1,14 @@
+package com.hengheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lkj
+ */
+@SpringBootApplication
+public class PermissionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PermissionApplication.class, args);
+    }
+}
