49794583f882de19f0930e6ed6d06a4f0cc138c6
liukejun <1337023373@qq.com>
2025-05-22 14:16:12 +0800

    feat:添加swagger3和lang3依赖 feat:添加统一异常处理类，统一返回类，字符串处理工具类

---
+++ src/main/java/com/hengheng/common/config/Swagger3Config.java
@@ -0,0 +1,61 @@
+package com.hengheng.common.config;
+ 
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+ 
+/**
+ * @Author BlueFire
+ * @email D102601560@gamil.com
+ * @Date 2020/10/24 -14:30
+ */
+@Configuration
+@EnableOpenApi
+public class Swagger3Config {
+    //可以配置在application文件中
+    /**
+     * 与Spring 同一级的配置
+     *swagger:
+     *   enable: true
+     *   application-name: ${spring.application.name}
+     *   application-version: 1.0
+     *   application-description: springfox swagger 3.0整合Demo
+     *   try-host: http://localhost:${server.port}
+     */
+
+    @Value("${springfox.documentation.swagger-ui.enabled}")
+    private Boolean swaggerEnabled;
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                // 是否开启
+                .enable(swaggerEnabled)
+                .select()
+                // 扫描的路径使用@Api的controller
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                // 指定路径处理PathSelectors.any()代表所有的路径
+                .paths(PathSelectors.any())
+                .build();
+    }
+ 
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Swagger3接口文档")
+                .description("适用于前后端分离统一的接口文档")
+                //作者信息
+                //.contact(new Contact("name","url", "email"))
+                //.contact(new Contact("hengheng","www.mooding.cn", "D102601560@gamil.com"))
+                .version("1.0")
+                .build();
+    }
+}
---
+++ src/main/java/com/hengheng/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.hengheng.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author lkj
+ * @Date 2025/5/22 13:14
+ * @Version 1.0
+ */
+
+@RestController
+@RequestMapping("/login")
+@Api(tags = "登录模块")
+public class LoginController {
+}
